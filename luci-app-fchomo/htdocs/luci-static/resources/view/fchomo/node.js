431271189842488ae7e6d7020acfced3c190d5da
Anya Lin <hukk1996@gmail.com>
2026-07-10 16:33:33 +0800

    chore: po

--- luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/node.js
+++ luci-app-fchomo/htdocs/luci-static/resources/view/fchomo/node.js
@@ -271,7 +271,7 @@ return view.extend({
 			const target_sub_rule = this.section.getOption('target_sub_rule').formvalue(section_id);
 
 			if (!value && !target_sub_rule)
-				return _('Expecting: %s or %s is required.').format(_('REMATCH-NAME marking'), _('Use sub rule'));
+				return _('Expecting: Least one of %s or %s.').format(_('REMATCH-NAME marking'), _('Use sub rule'));
 
 			return hm.validateAuthUsername.call(this, section_id, value);
 		};
@@ -289,7 +289,7 @@ return view.extend({
 			const target_rematch_name = this.section.getOption('target_rematch_name').formvalue(section_id);
 
 			if (!value && !target_rematch_name)
-				return _('Expecting: %s or %s is required.').format(_('REMATCH-NAME marking'), _('Use sub rule'));
+				return _('Expecting: Least one of %s or %s.').format(_('REMATCH-NAME marking'), _('Use sub rule'));
 
 			return true;
 		};
